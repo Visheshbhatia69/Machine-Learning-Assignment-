f6aaf60740682130eafd0612696a5d1f8a664a57
Visheshbhatia69 <129662705+Visheshbhatia69@users.noreply.github.com>
2025-12-01 22:59:39 +0000

    Update MLassignment.py

--- MLassignment.py
+++ MLassignment.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
 
-#loading and checking data 
+#loading and checking data
 df = pd.read_csv("wallacecommunications.csv")
 
 print(df.head())
 print(df.tail())
 print(df.info())
 print(df['new_contract_this_campaign'].value_counts())   # checking imbalance
+
+#dropping unnecessary coloums
+df = df.drop(['ID','town','country'],axis=1)
+
+#converting target coloum
+df['new_contract_this_campaign'] = df['new_contract_this_campaign'].replace({'yes':1,'no':0})
